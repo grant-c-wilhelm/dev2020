b0ec16582ab231dafbc6aa2e2db0e19d43f3274e
Grant Wilhelm <grant.c.wilhelm@gmail.com>
2020-03-23 14:17:53 -0600

    playing aroud with gatsbyjs

---
+++ gatsbyjs-project/src/hello-world/src/pages/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+import {Link} from "gatsby"
+export default () => <div style={{ color: "purple" }}>
+    <h1  >Hello Gatsby</h1>
+    <p>What an amazing world we are living in.</p>
+    <img  src="https://images.pexels.com/photos/1123972/pexels-photo-1123972.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=450&w=540" alt=""  />
+    <Link to="/contact/">Contact</Link>
+</div>
